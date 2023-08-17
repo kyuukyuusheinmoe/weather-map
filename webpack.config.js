83a5d3d7a4fafeea9f9059d430965f071ab01c22
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2023-08-17 15:42:52 +0630

    add tailwind setup

--- webpack.config.js
+++ webpack.config.js
@@ -72,6 +72,17 @@ module.exports={
                 test: /\.(js|jsx)$/,    //kind of file extension this rule should look for and apply in test
                 exclude: /node_modules/, //folder to be excluded
                 use:  'babel-loader' //loader which we are going to use
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i, 
+                use: 'file-loader',
+                options: {
+                  name: '/public/icons/[name].[ext]'
+                }
             }
         ]
     }
